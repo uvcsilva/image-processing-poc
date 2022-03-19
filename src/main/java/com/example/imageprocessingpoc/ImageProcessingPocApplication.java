8b94163698cc62aefd1ed5961c951b8f7b1156c3
vitor silva <uelinton.silva@compasso.com.br>
2022-03-19 10:10:09 -0300

    build

---
+++ src/main/java/com/example/imageprocessingpoc/ImageProcessingPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.imageprocessingpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageProcessingPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageProcessingPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imageprocessingpoc/ImageProcessingPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imageprocessingpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageProcessingPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
